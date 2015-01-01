0421f98f2086aa0950b42217e8b969852ffdd148
Timothy Escopete #local #osxy <me@timothyae.com>
2015-01-01 15:19:46 +0800

    Update changes in lemp.sh file;
    * add more lines;

--- lemp.sh
+++ lemp.sh
@@ -12,10 +12,16 @@ sudo apt-get update &&
   mkdir /var/www &&
   mkdir /var/www/default &&
   mkdir /var/www/default/html &&
+  sudo git init /etc/nginx/sites-available/ &&
+  cd /etc/nginx/sites-available/ &&
+  sudo git add . &&
+  sudo git commit -m "Original state;" &&
+  cd ~ &&
   sudo sed -i -e 's/root \/usr\/share\/nginx\/html;/root \/var\/www\/default\/html;/g' /etc/nginx/sites-available/default &&
   sudo sed -i -e 's/index index.html index.htm;/index index.php index.html index.htm;/g' /etc/nginx/sites-available/default &&
   sudo sed -i -e 's/#error_page 404 \/404.html;/error_page 404 \/404.html;/g' /etc/nginx/sites-available/default &&
   sudo sed -i -e 's/#error_page 500 502 503 504 \/50x.html;/error_page 500 502 503 504 \/50x.html/g;' /etc/nginx/sites-available/default &&
   sudo sed -i -e '/#location = \/50x.html {/{n;N;d}' /etc/nginx/sites-available/default &&
-  sudo sed -i -e '/#location = \/50x.html {/a \\\t\t root \/var\/www\/default\/html;/a \\\t }' /etc/nginx/sites-available/default &&
+  sudo sed -i -e '/#location = \/50x.html {/a \\t}' /sites/nginx/sites-available/default &&
+  sudo sed -i -e '/#location = \/50x.html {/a \\t\troot \/var\/www\/default\/html;' /etc/nginx/sites-available/default &&
   sudo sed -i -e 's/#location = \/50x.html {/location = \/50x.html {/g' /etc/nginx/sites-available/default
