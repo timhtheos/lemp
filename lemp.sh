bb9b1deb9f8d10f086357212f7dfcf6a7cc01458
Timothy Escopete #local #osxy <me@timothyae.com>
2015-01-01 15:02:57 +0800

    Correct missing /g in s/ command;

--- lemp.sh
+++ lemp.sh
@@ -15,7 +15,7 @@ sudo apt-get update &&
   sudo sed -i -e 's/root \/usr\/share\/nginx\/html;/root \/var\/www\/default\/html;/g' /etc/nginx/sites-available/default &&
   sudo sed -i -e 's/index index.html index.htm;/index index.php index.html index.htm;/g' /etc/nginx/sites-available/default &&
   sudo sed -i -e 's/#error_page 404 \/404.html;/error_page 404 \/404.html;/g' /etc/nginx/sites-available/default &&
-  sudo sed -i -e 's/#error_page 500 502 503 504 \/50x.html;/error_page 500 502 503 504 \/50x.html;' /etc/nginx/sites-available/default &&
+  sudo sed -i -e 's/#error_page 500 502 503 504 \/50x.html;/error_page 500 502 503 504 \/50x.html/g;' /etc/nginx/sites-available/default &&
   sudo sed -i -e '/#location = \/50x.html {/{n;N;d}' /etc/nginx/sites-available/default &&
   sudo sed -i -e '/#location = \/50x.html {/a \\\t\t root \/var\/www\/default\/html;/a \\\t }' /etc/nginx/sites-available/default &&
   sudo sed -i -e 's/#location = \/50x.html {/location = \/50x.html {/g' /etc/nginx/sites-available/default
