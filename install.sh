65aa5ac91fb794c941b07940844c832123789f58
Timothy Escopete #local #osxy <me@timothyae.com>
2015-01-01 15:29:53 +0800

    Add global config for git; and correct typo /sites/ to /etc/;

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 sudo apt-get update &&
   sudo apt-get upgrade -y &&
   sudo apt-get install vim git -y &&
+  sudo git config --global user.email "none@none.none" &&
+  sudo git config --global user.name "None" &&
   sudo apt-get install nginx -y &&
   sudo apt-get install mysql-server -y &&
   sudo mysql_install_db &&
@@ -22,6 +24,6 @@ sudo apt-get update &&
   sudo sed -i -e 's/#error_page 404 \/404.html;/error_page 404 \/404.html;/g' /etc/nginx/sites-available/default &&
   sudo sed -i -e 's/#error_page 500 502 503 504 \/50x.html;/error_page 500 502 503 504 \/50x.html/g;' /etc/nginx/sites-available/default &&
   sudo sed -i -e '/#location = \/50x.html {/{n;N;d}' /etc/nginx/sites-available/default &&
-  sudo sed -i -e '/#location = \/50x.html {/a \\t}' /sites/nginx/sites-available/default &&
+  sudo sed -i -e '/#location = \/50x.html {/a \\t}' /etc/nginx/sites-available/default &&
   sudo sed -i -e '/#location = \/50x.html {/a \\t\troot \/var\/www\/default\/html;' /etc/nginx/sites-available/default &&
   sudo sed -i -e 's/#location = \/50x.html {/location = \/50x.html {/g' /etc/nginx/sites-available/default
